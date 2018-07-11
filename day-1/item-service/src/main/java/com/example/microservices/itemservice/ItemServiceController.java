63cafbe1c8ef2ee15faa6fd1bd02af1af6ce8049
Dipanjan Bhowmik <onlydevelop@gmail.com>
2018-07-11 20:28:30 +0530

    Step 7: Add refresh scope

--- day-1/item-service/src/main/java/com/example/microservices/itemservice/ItemServiceController.java
+++ day-1/item-service/src/main/java/com/example/microservices/itemservice/ItemServiceController.java
@@ -3,11 +3,13 @@ package com.example.microservices.itemservice;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ItemServiceController {
     
     @Autowired
